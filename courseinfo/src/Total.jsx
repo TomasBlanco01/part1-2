7ecf01e32de62bd559ef0c4b2aea951223498107
TomasBlanco01 <tomasblancooUTN@gmail.com>
2025-05-20 18:58:48 -0300

    ej 1.5 terminado

--- courseinfo/src/Total.jsx
+++ courseinfo/src/Total.jsx
@@ -1,4 +1,4 @@
-const Total = ({ parts={total} }) => {
+const Total = ({ parts }) => {
     return (
     <p>Number of exercises: {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
     )
