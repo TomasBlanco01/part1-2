6c11578720ffffa4f18f9939eef40ee13657fd1c
TomasBlanco01 <tomasblancooUTN@gmail.com>
2025-05-21 20:30:17 -0300

    ej 1.9

--- unicafe/src/Statistics.jsx
+++ unicafe/src/Statistics.jsx
@@ -6,15 +6,17 @@ const Statistics = ({good, neutral, bad}) => {
     var average = ((good - bad) / all)
     var positive = ((good * 100) / all)
 
-    return (
+    return ( all > 0 ? (
         <div>
             <h1>Statistics</h1>
             <p></p>
             <p>Good {good}<br /> Neutral {neutral}<br /> Bad {bad}</p>
             <p></p>
             <p>All {all}<br /> Average {average}<br /> Positive {positive} %</p>
-        </div>
-    )
+        </div>) : (
+            <p>No feedback given.</p>
+          )
+        )
 }
 
 export default Statistics
