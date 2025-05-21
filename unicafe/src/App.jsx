290dd000a2e85bc11b29c9a5fc05292476c2322c
TomasBlanco01 <tomasblancooUTN@gmail.com>
2025-05-21 19:59:37 -0300

    ej 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  var all = (good + neutral + bad)
+  var average = ((good - bad) / all)
+  var positive = ((good * 100) / all)
 
   return (
     <div>
@@ -16,9 +19,9 @@ const App = () => {
       <p></p>
       <h1>Statistics</h1>
       <p></p>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
+      <p>Good {good}<br/> Neutral {neutral}<br/> Bad {bad}</p>
+      <p></p>
+      <p>All {all}<br/> Average {average}<br/> Positive {positive}</p>
     </div>
   )
 }
