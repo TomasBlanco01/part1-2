f108048251f4661e28ada05df9cdd77983f7bd38
TomasBlanco01 <TomasblancooUTN@gmail.com>
2025-08-13 15:41:54 -0300

    ej 2.8

--- agenda/src/App.jsx
+++ agenda/src/App.jsx
@@ -2,20 +2,42 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '3388-418735'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handlePersons = (event) => {
     event.preventDefault()
-    if (newName.trim() === '') return setNewName('')
-    if (persons.some((person) => (person.name == newName))) return alert(`${newName} is already added to phonebook`)
-    setPersons([...persons, { name: newName }])
+    if (newName.trim() === '' || newNumber.trim() === ''){
+      alert('Please fill in both name and number') 
+      setNewName('')
+      setNewNumber('')
+      return
+    } 
+    if (persons.some((person) => (person.name.toLowerCase() === newName.toLowerCase()))){
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+      return
+    }
+    if (persons.some((person) => (person.number === newNumber))){
+      alert(`${newNumber} is already added to phonebook`)
+      setNewNumber('')
+      return
+    }
+    setPersons([...persons, { name: newName, number: newNumber}])
     setNewName('')
+    setNewNumber('')
   }
   
   const handleName = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -26,12 +48,15 @@ const App = () => {
           name: <input value={newName} onChange={handleName}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumber}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
         {persons.map((person) => (
-          <div>{person.name}</div>
+          <div>{person.name} {person.number}</div>
         ))}
     </div>
   )
