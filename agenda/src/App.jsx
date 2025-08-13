e600413e7b0ac8e66dd3a4d49c81f7fa8f779a5a
TomasBlanco01 <TomasblancooUTN@gmail.com>
2025-08-13 11:47:31 -0300

    ej 2.6

---
+++ agenda/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handlePersons = (event) => {
+    event.preventDefault()
+    if (newName.trim() === '') return
+    setPersons([...persons, { name: newName }])
+    setNewName('')
+  }
+  
+  const handleName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handlePersons}>
+        <div>
+          name: <input value={newName} onChange={handleName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map((person) => (
+          <div>{person.name}</div>
+        ))}
+    </div>
+  )
+}
+
+export default App
